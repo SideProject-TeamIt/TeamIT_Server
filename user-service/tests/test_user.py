f7137ab7d9feff10350ad257d801c99c80f35ce5
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-04-01 07:24:47 +0900

    Feature : 기본 test 코드 작성

---
+++ user-service/tests/test_user.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.json() == {"message": "여기는 user_service 페이지입니다"}
